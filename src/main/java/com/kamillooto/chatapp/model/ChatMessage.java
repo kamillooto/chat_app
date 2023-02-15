2054d03a05fb9bdaf7a5b383af5c792708b58cdc
Kamil Otocki <kamillooto@gmail.com>
2023-02-15 18:05:28 +0100

    Create ChatMessage model class

---
+++ src/main/java/com/kamillooto/chatapp/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.kamillooto.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+
+    private String message;
+    private String user;
+
+}
