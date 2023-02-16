aa70b5c2c3bba84191f39acc2002a09d0a586897
Kamil Otocki <kamillooto@gmail.com>
2023-02-16 17:31:39 +0100

    Add connect() on load page, sendMessage() on click button to index.html and fix chat.js

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -26,7 +26,7 @@ function sendMessage() {
 var messageToSend = document.getElementById('messageToSend').value;
 var user = document.getElementById('user').value;
 
-client.send("/app/chat"), {}, JSON.stringify({'message': messageToSend, 'user': user});
+client.send("/app/chat", {}, JSON.stringify({'message': messageToSend, 'user': user}));
 document.setElementById('messageToSend').value = "";
 
 }
