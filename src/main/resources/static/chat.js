8ed7ef32e2974838b6e3772b6d1cd3aaa67ab629
Kamil Otocki <kamillooto@gmail.com>
2023-02-16 16:31:04 +0100

    Add connect, showMessage and sendMessage functions to chat.js

---
+++ src/main/resources/static/chat.js
@@ -0,0 +1,32 @@
+var client = null;
+
+function connect() {
+    client = Stomp.client("ws://localhost:8080/chat")
+
+    client.connect({}, function (frame) {
+    client.subscribe("/topic/chat", function (message) {
+        showMessage(JSON.parse(message.body).message, JSON.parse(message.body).user)
+        });
+    })
+}
+
+function showMessage(message, user) {
+    var newResponse = document.createElement('p')
+
+    newResponse.appendChild(document.createTextNode(user))
+    newResponse.appendChild(document.createTextNode(": "))
+    newResponse.appendChild(document.createTextNode(message))
+
+    var response = document.getElementById('response')
+    response.appendChild(newResponse)
+}
+
+function sendMessage() {
+
+var messageToSend = document.getElementById('messageToSend').value;
+var user = document.getElementById('user').value;
+
+client.send("/app/chat"), {}, JSON.stringify({'message': messageToSend, 'user': user});
+document.setElementById('messageToSend').value = "";
+
+}
